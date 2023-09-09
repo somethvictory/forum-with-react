a4056f567514b8e1c21007c85dcdd22b1a32b7dc
Victory <victory.someth@gmail.com>
2023-09-09 15:42:21 +0800

    Add devise

--- Gemfile
+++ Gemfile
@@ -49,6 +49,7 @@ gem "bootsnap", require: false
 # gem "image_processing", "~> 1.2"
 
 gem 'react-rails'
+gem 'devise'
 
 group :development, :test do
   gem 'pry'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
   validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
 
   has_many :questions
